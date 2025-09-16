69661c254fe8d79c4abd3bb92550564b82a71564
Rodolfo Sanches <rodolfo.sanches@nicolasec.com.br>
2025-09-15 22:23:44 -0300

    Arrumado a interface Front-end

--- src/main/resources/static/js/user_info.js
+++ src/main/resources/static/js/user_info.js
@@ -1,18 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const userNameElement = document.querySelector('.fw-bold');
-    const welcomeUserElements = document.querySelectorAll('.d-flex.flex-column.lh-1 span.fw-bold');
+    const userNameMobileElement = document.getElementById('user-name-mobile');
+    const userNameDesktopElement = document.getElementById('user-name-desktop');
     const loggedInUser = sessionStorage.getItem('loggedInUser');
 
     if (loggedInUser) {
-        // Atualiza o nome de usuário na versão mobile
-        if (userNameElement) {
-            userNameElement.textContent = loggedInUser;
+        if (userNameMobileElement) {
+            userNameMobileElement.textContent = loggedInUser;
         }
         
-        // Atualiza o nome de usuário na versão desktop
-        welcomeUserElements.forEach(element => {
-            element.textContent = loggedInUser;
-        });
+        if (userNameDesktopElement) {
+            userNameDesktopElement.textContent = loggedInUser;
+        }
 
     } else {
         // Se não houver usuário logado, redireciona para a página de login
